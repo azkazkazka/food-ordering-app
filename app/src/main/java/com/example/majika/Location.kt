cc26cfd965a157be12da6cd55da6c24cefaddeb6
sarahazka <13520083@std.stei.itb.ac.id>
2023-02-08 23:47:26 +0700

    fix: location recyclerview

--- app/src/main/java/com/example/majika/Location.kt
+++ app/src/main/java/com/example/majika/Location.kt
@@ -1,26 +1,21 @@
 package com.example.majika
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
+import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import retrofit2.Retrofit
 
 
 class Location : Fragment() {
+    private val menuModels = ArrayList<MenuModel>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        val recyclerView: RecyclerView = findViewById(R.id.mRecyclerView)
-
-        populateList()
-
-        val adapter: LocationRVAdapter = LocationRVAdapter(this.menuModels)
-        recyclerView.adapter = adapter
-        recyclerView.layoutManager = LinearLayoutManager(this)
-
-        getResponse()
     }
 
     private fun populateList() {
@@ -44,11 +39,7 @@ class Location : Fragment() {
                     println("BISAAAAjioj")
 
                 } else {
-                    Toast.makeText(
-                        this@MainActivity,
-                        response.errorBody().toString(),
-                        Toast.LENGTH_LONG
-                    ).show()
+
                 }
             }catch (Ex:Exception){
                 Log.e("Error",Ex.localizedMessage)
@@ -62,6 +53,19 @@ class Location : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_location, container, false)
+        val view: View = inflater.inflate(R.layout.fragment_location, container, false)
+        val recyclerView: RecyclerView = view.findViewById(R.id.mRecyclerView)
+
+        populateList()
+
+        val adapter: LocationRVAdapter = LocationRVAdapter(this.menuModels)
+        recyclerView.adapter = adapter
+        recyclerView.layoutManager = LinearLayoutManager(this.context)
+
+        getResponse()
+        return view
+    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
     }
 }
